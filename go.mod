2b06a4e07496c2de137767ab3230fdc800035439
Alexander Shuliakovsky <shuliakovsky@gmail.com>
2025-09-03 22:58:12 +0300

    main - fix tests

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.0
 require (
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
+	github.com/joho/godotenv v1.5.1
 	github.com/prometheus/client_golang v1.23.0
 	github.com/stretchr/testify v1.10.0
 	github.com/swaggo/http-swagger v1.3.4
--- pkg/health/health_test.go
+++ pkg/health/health_test.go
@@ -1,16 +1,25 @@
 package health
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/shuliakovsky/rpc-forwarder/pkg/networks"
 	"github.com/shuliakovsky/rpc-forwarder/pkg/registry"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
+
+func TestMain(m *testing.M) {
+	_ = godotenv.Load(".env")
+	os.Exit(m.Run())
+}
 
 func newTestChecker() *Checker {
 	logger, _ := zap.NewDevelopment()
@@ -31,16 +40,45 @@ func TestCheckEVM_OK(t *testing.T) {
 	require.GreaterOrEqual(t, ping, int64(0), "ping should be non-negative")
 }
 
-func TestCheckBTC_Blockstream_OK(t *testing.T) {
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("123456"))
-	}))
-	defer srv.Close()
-
+func TestCheckBTC_AllVariants(t *testing.T) {
 	h := newTestChecker()
-	ok, _ := h.checkBTC(networks.Node{URL: srv.URL}, 2*time.Second)
-	require.True(t, ok, "BTC node should be alive")
+
+	t.Run("Blockstream variant", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, "/api/blocks/tip/height", r.URL.Path)
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte("123456"))
+		}))
+		defer srv.Close()
+
+		ok, _ := h.checkBTC(networks.Node{URL: srv.URL + "/api"}, 2*time.Second)
+		require.True(t, ok)
+	})
+
+	t.Run("Tatum gateway variant", func(t *testing.T) {
+		apiKey := os.Getenv("TATUM_API_KEY")
+		if apiKey == "" {
+			t.Skip("TATUM_API_KEY not set in environment")
+		}
+
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, http.MethodPost, r.Method)
+			require.Equal(t, apiKey, r.Header.Get("x-api-key"))
+			body, _ := io.ReadAll(r.Body)
+			require.Contains(t, string(body), `"method":"getblockcount"`)
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(`{"jsonrpc":"2.0","id":1,"result":123456}`))
+		}))
+		defer srv.Close()
+
+		tatumURL := strings.Replace(srv.URL, "127.0.0.1", "gateway.tatum.io", 1)
+
+		ok, _ := h.checkBTC(networks.Node{
+			URL:     tatumURL,
+			Headers: map[string]string{"x-api-key": apiKey},
+		}, 2*time.Second)
+		require.True(t, ok, "Tatum gateway node should be alive")
+	})
 }
 
 func TestUpdateNetwork_Prioritizes(t *testing.T) {
